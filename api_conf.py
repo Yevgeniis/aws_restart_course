73b7ae8cbc92d82b94875c21e5d060d61e6bac41
Yevgenii <yevgenii@bootcampappleseeds.com>
2022-10-11 19:29:31 +0300

    class,add git_image_url and get_movieid

--- api_conf.py
+++ api_conf.py
@@ -1,6 +1,6 @@
 import requests
+import imdb
 import os
-
 
 class mvdb:
 
@@ -13,13 +13,42 @@ class mvdb:
         self.sizes = self.config['images']['poster_sizes'][-1]
         self.IMG_PATTERN = 'http://api.themoviedb.org/3/movie/{imdbid}/images?api_key={KEY}'
 
-        def get_movieID():
-            pass
+    def get_movieid(self,movie_name):
+        name = movie_name
+        ia = imdb.Cinemagoer()
+        search = ia.search_movie(name)
+        self.movieid = "tt" + str(search[0].movieID)
+        return self.movieid
 
-        def get_image():
-            self.IMG_PATTERN.format(KEY=self.KEY,imdbid=)
-            pass
+    def get_image_url(self,imdbid):
+        self.imgurl = self.IMG_PATTERN.format(KEY=self.KEY,imdbid=imdbid)
+        r = requests.get(self.imgurl)
+        self.api_response = r.json()
 
-mv=mvdb()
+        posters = self.api_response['posters']
+        poster_urls = []
+        for poster in posters:
+            imagename = poster['file_path']
+            url = "{0}{1}{2}".format(self.base_url, max_size, imagename)
+            poster_urls.append(url)
+        return self.imgurl
+        pass
+
+    def download_image(self,imageurl):
+        pass
+
+mv = mvdb()
+imdb = mv.get_movieid("matrix")
+url = mv.get_image_url(imdb)
+mv.download_image(url)
 
 
+#mv.get_image(movieid)
+#mv.get_
+#print(mv)
+
+
+#name = 'The Lord of the Rings: The Fellowship of the Ring'
+
+
+#print(id)
