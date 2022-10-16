b9b3abca25f2ee482ed80ce64bfce1432a23596e
Mohammad-Joubat <68898478+Mohammad-Joubat@users.noreply.github.com>
2022-10-16 13:11:54 +0300

    commit from mohammad 10/16/2022  --> 13:11

--- api_project/flask_web.py
+++ api_project/flask_web.py
@@ -15,24 +15,27 @@ def about():
 @app.route('/search',methods=['GET', 'POST'])
 
 def load_insert_html():
-    if  request.method == 'POST':
-        movie_name = request.form['name']
-        imdb = mdb.get_movieid(movie_name)
-        if mdb.find_data():
-            binary_file = mdb.read_data()
-            try:
+    try:
+        if request.method == 'POST':
+            movie_name = request.form['name']
+
+            imdb = mdb.get_movieid(movie_name)
+            if mdb.find_data():
+                binary_file = mdb.read_data()
                 image = b64encode(binary_file).decode("utf-8")
                 src = "data:image/gif;base64," + image
-                return f'<center><img src={src} alt="{movie_name}" width="300" height="400"></center>'
-            except:
-                return render_template('error.html')
-        elif type(mdb.find_data()) != str :
-            return render_template('error.html')
-        else:
-            return render_template('error.html')
+                return f'<img src={src} alt="{movie_name}" width="300" height="400">'
+
+            mdb.get_image_url(imdb)
+            mdb.getPosterFile()
+            mdb.insert_data()
+            image = b64encode(mdb.f_bin).decode("utf-8")
+            src = "data:image/gif;base64," + image
+            return f'<img src={src} alt="{movie_name}" width="300" height="400">'
+    except:
+        return f"This  Poster Not Found"
     return render_template('search.html')
 
 
-if __name__=="__main__":
-    app.run(port=80,host="0.0.0.0")
-
+if __name__ == "__main__":
+    app.run(port=80, host="0.0.0.0")
