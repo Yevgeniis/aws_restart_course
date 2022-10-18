542afc11aa9c83053852f47232f463fa97fc5688
unknown <sm_nl@hotmail.com>
2022-10-18 18:58:24 +0300

    The new version of file <flask_test.py> by Nelly

--- flask_test.py
+++ flask_test.py
@@ -10,28 +10,17 @@ import pytest
 
 app = Flask(__name__,template_folder="Templates")
 
-file_handler = FileHandler('errorlog.txt')
-file_handler.setLevel(WARNING)
 
-#rollbar.init('POAT_SERVER_ITEM_ACCESS_TOKEN', 'dev')
-
-# @app.errorhandler(InternalServerError)
-# def payload_validation_failure(err):
-#      return jsonify({"message": "ERROR NAME, PLEASE,TRY AGAIN !!!"}),500
-#      return jsonify({"message": "Server attempted to return invalid data"}), 500
 
 @app.errorhandler(InternalServerError)
 def validation_failure(err):
   return jsonify({"ERROR NAME !!!":" ---> PLEASE,TRY AGAIN !!!"}),500
 
-#
-# @app.route("/profile/<name>")
-# def profile(name):
-#   return render_template("index.html", name=name)
+
 
 @app.route('/')
 def index():
-    return render_template('search.html')  #("index.html")
+    return render_template('search.html')
 
 @app.route('/about')
 def about():
@@ -46,13 +35,9 @@ def load_insert_html():
         movie_name = request.form['name']
         imdb = mdb.get_movieid(movie_name)
 
-    # if type(movie_name) != str:
-    #      movie_name = "ERROR NAME"
-    #      print(movie_name)
 
 
-
-    if mdb.find_data():   #!= False:
+    if mdb.find_data():
 
             binary_file = mdb.read_data()
 
@@ -60,22 +45,7 @@ def load_insert_html():
             src = "data:image/gif;base64," + image
             return f'<center><img src={src} alt="{movie_name}" width="300" height="400"></center>'
 
-# except: #TypeError:
-# rollbar.report_message('There is a data type mismatch', 'fatal')
- #    except:
- # rollbar.report_exc_info()
 
-
-    # except:
-    #  #return render_template('Error.html')
-    #   if not isinstance(image):  #(type(mdb.find_data()) != str) :
-    #         return render_template('Error.html')
-    #  #elif :
-    ##return render_template('Error.html')
-
- # assert isinstance(mdb.name,str), "a should be nonzero!"
-# if not isinstance(mdb.name,str):
-#     raise ValueError("name should be string")
 
     mdb.get_image_url(imdb)
     mdb.getPosterFile()
@@ -84,8 +54,6 @@ def load_insert_html():
     src = "data:image/gif;base64," + image
 
 
-    # except ValueError as e:
-    #   print(e)
 
     return f'<center><img src={src} alt="{movie_name}" width="300" height="400"></center>'
     return render_template('search.html')
